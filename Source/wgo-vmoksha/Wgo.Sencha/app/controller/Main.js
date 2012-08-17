0329065544d817b5d6c17b83c6eb76b80908f259
vmmobility <vmmobility@62e6b382-633c-7ef0-cd8d-8fe3c8f30c53>
2012-08-17 12:53:52 +0000

    Added Logout functionality to home page and changed JSONP request to Ajax request

--- Source/wgo-vmoksha/Wgo.Sencha/app/controller/Main.js
+++ Source/wgo-vmoksha/Wgo.Sencha/app/controller/Main.js
@@ -9,7 +9,8 @@ Ext.define('Wgo.controller.Main', {
             //Attach event handlers for controls matched by component queries. In this case button whose action="btnLoginSubmit" is queried
             // and for the query result, event handler function "submitLoginForm" is attached
             'button[action=btnLoginSubmit]' : {tap:"submitLoginForm"}, //Uses ComponentQuery selector to find the matching
-            'button[action=btnDashboardClick]' : {tap:"btnDashboardClick"} //Uses ComponentQuery selector to find the matching
+            'button[action=btnDashboardClick]' : {tap:"btnDashboardClick"},
+            'button[action=btnDashboardLogoutClick]' : {tap:"btnDashboardLogoutClick"} //Uses ComponentQuery selector to find the matching
         }
     },
     //------------------------------------------------------------------------------------------------------------------
@@ -28,7 +29,7 @@ Ext.define('Wgo.controller.Main', {
         var user = form.getValues().txtUserName;
         var pwd = form.getValues().txtPassword;
         console.log("Before Form Submit")
-        Ext.util.JSONP.request({
+        /*Ext.util.JSONP.request({
             url: 'http://wgo-1.apphb.com/authenticate',
             dataType: "jsonp",
             params: {
@@ -49,6 +50,17 @@ Ext.define('Wgo.controller.Main', {
                 Ext.Viewport.setActiveItem({xtype:'main'},{type: 'slide', direction: 'right'});
                 //Ext.Msg.alert("Network Failure or Time out happened");
             }
+        });*/
+        Ext.Ajax.request({            
+            url: 'http://localhost:3000/users/authenticate',
+            method: 'POST',
+            type:'json',
+            params: {
+                username: user,
+                password: pwd
+            },
+            callback: this.onAuthenticateCallback, //Why do we add a callback? I should read but it's to tell the client that the Server is ready to send data back? But this seems like we are calling a fn.
+            scope: this //What is scope? The scope in which the action should be called.  Unclear what this means. 
         });
         //console.log("login button tap event (End)");
     },
@@ -58,5 +70,72 @@ Ext.define('Wgo.controller.Main', {
         console.log("btnDashboardClick (Start)")
         Ext.getCmp('idMain').setActiveItem(1,{type: 'slide', direction: 'right'}).getTabBar().show();
         console.log("btnDashboardClick (End)")
-    }
+    },
+    onAuthenticateCallback: function(options, success, response) {
+        console.log(success); 
+        console.log(response.responseText);
+        // Parsing the responseText as an object
+        var usr = JSON.parse(response.responseText);
+        debugger;
+        if(success == true) {
+            Ext.Viewport.unmask();
+            //Stringifying the parsed object and storing it in localStorage
+            localStorage.setItem('userData',JSON.stringify(usr.Data));
+            var getUserData = localStorage.getItem('userData');
+            localStorage.setItem('rememberUserLogged',getUserData);
+            localStorage.setItem('rememberUser',1);
+            Ext.Viewport.setActiveItem({xtype:'main'},{type: 'slide', direction: 'right'});
+        }
+        else {
+            Ext.Viewport.unmask();
+            Ext.Msg.alert("Unauthorized access");
+        }
+    },
+    //Logout click functionality
+    btnDashboardLogoutClick:function(){
+        console.log("btnDashboardClick (Start)")
+        var logVal = localStorage.getItem('rememberUser');
+        var msg = new Ext.MessageBox();
+        console.log(logVal);        
+        if(logVal == 1)
+            {
+                localStorage.setItem('rememberUser',0);
+                var getLoggedInData = localStorage.getItem('rememberUserLogged');
+                var parseLoggedInData = JSON.parse(getLoggedInData);
+                Ext.Viewport.setMasked(false);                
+                msg.show({
+                    title: 'Logout',
+                    message: 'Logged Out Successfully!',
+                    ui:'light',
+                    cls: 'vm_success',
+                    showAnimation: 'fadeIn',
+                    hideAnimation: 'fadeOut',
+                    buttons: [{text:'OK',itemId:'ok'}],
+                    fn:function(){
+                        localStorage.removeItem("rememberUserLogged");                        
+                        localStorage.removeItem('userData');
+                        window.location.reload();
+                    }
+                });
+            }
+        else 
+            {
+                Ext.Viewport.setMasked(false);
+                msg.show({
+                        title: 'Logout',
+                        message: 'Logged Out Successfully!',
+                        ui:'light',
+                        cls: 'vm_success',
+                        showAnimation: 'fadeIn',
+                        hideAnimation: 'fadeOut',
+                        buttons: [{text:'OK',itemId:'ok'}],
+                        fn:function(){
+                            window.location.reload();
+                        }
+                    });
+            }
+        console.log("btnDashboardClick (End)")
+    },
+            
+        
 });
