a2c8606e7b71655c3f1925d358dc13e13672b1e2
vmmobility <vmmobility@62e6b382-633c-7ef0-cd8d-8fe3c8f30c53>
2012-08-23 11:25:44 +0000

    Modified Main Controller with syncing of online to offline store on tap of tabBar item Modified UserController.js with syncing of online to offline store to display the changes for Add/Edit

--- trunk/Source/wgo-vmoksha/Wgo.Sencha/app/controller/UserController.js
+++ trunk/Source/wgo-vmoksha/Wgo.Sencha/app/controller/UserController.js
@@ -12,7 +12,7 @@ Ext.define('Wgo.controller.UserController', {
             'button[action=btnUserSubmit]' : {tap:"btnUserSubmitClick"},
             '#idUserList': {disclose: 'showEdit'} //"#idUserList" is a component query (can say dom selector)
         }
-    },
+    },    
     //------------------------------------------------------------------------------------------------------------------
     init: function() {
         console.log("User controller init(Start)")
@@ -29,11 +29,38 @@ Ext.define('Wgo.controller.UserController', {
     },
     showUserList: function() {
         console.log("User controller showUserList(Start)")
-        //debugger;
+        // Syncing Online to Offline again to make sure that the changes are shown
+        console.log('Syncing data from OnlineUserStore to OfflineUserStore');
+        // Getting the online store and storing it in a local variable
+        var onlineStore = Ext.getStore('OnlineUserStore');
+        // Getting the offline store and storing it in a local variable
+        var offlineStore = Ext.getStore('OfflineUserStore'); 
+        //Adding an listener called 'load' to the Online Store        
+        onlineStore.addListener('load', function () {            
+            //Copying records from Online Store to Offline Store
+            this.each(function (record) {
+                //For proper syncing of data this is required
+                //when the store has unchanged data being synced the below must be used to sync the data
+                //record.phantom = true; // POST Operation
+                //Reference URL -> http://stackoverflow.com/questions/11360028/problems-with-store-syncing
+                record.dirty = true;//PUT Operation
+                //Adding data to Offline Store
+                offlineStore.add(record.data)[0];
+            });
+            //Syncing data to offline store
+            offlineStore.sync();
+            console.log(Ext.getCmp('idUserList'));
+            //Setting store to offlineStore
+            Ext.getCmp('idUserList').setStore(offlineStore);
+        });
+        // Loading the online store
+        onlineStore.load();
+        // Popping out the Add/Edit User form 
         this.getUserlist().pop();
+        // Toggling the Back to Add button
         Ext.getCmp('idBtnAdd').show();
-        Ext.getCmp('idBtnBack').hide();
-        console.log("User controller showUserList(End)")
+        Ext.getCmp('idBtnBack').hide();         
+        console.log("User controller showUserList(End)");
     },
     btnUserSubmitClick: function() {
         //debugger;
@@ -73,38 +100,55 @@ Ext.define('Wgo.controller.UserController', {
         console.log("User controller btnUserSubmitClick (End)")
     },
     onAddUserCallback: function(options, success, response) {
-
-        Ext.getCmp('userForm').setMasked(false);
-        Ext.getStore('UserStore').load();
-        //Ext.Viewport.setActiveItem(Ext.getCmp('userlist'));
-        this.showUserList();
-
+        console.log(success);
+        var userString;        
+        var username = Ext.getCmp('txtUser').getValue(), // Get form value by Dom identifier
+            password = Ext.getCmp('txtPwd').getValue(),
+            email = Ext.getCmp('txtEmail').getValue(),
+            id = Ext.getCmp('txtUserId').getValue() 
+        if(success==true)
+        {
+            Ext.getCmp('userForm').setMasked(false);
+            //Ext.getStore('UserStore').load();
+            //Ext.Viewport.setActiveItem(Ext.getCmp('userlist'));
+            // Calling the showUserList function to pop the Add/Edit User form and display the UserList
+            this.showUserList();
+        }
+        else
+        {
+            userString +='{"id":"'+id+'","username":"'+username+'","password":"'+password+'","email":"'+email+'"}';
+            console.log(userString);
+            console.log('true');
+            var records=[];
+            records[0]=JSON.parse(userString);
+            localStorage['records']=JSON.stringify(records[0]);
+        }
     },
     //------------------------------------------------------------------------------------------------------------------
     showEdit: function(list, record) {
-     //debugger;
-    //Todo: Detailsview not showingup after first attempt
-    console.log("User controller showEdit(Start)")
-    var auv = Ext.create("Wgo.view.AddUser");
-        auv.setValues({
-            txtUserId: record.get("id"),
-            txtUser: record.get("username"),
-            txtEmail: record.get("email"),
-            txtPwd: record.get("password")
-    });
-        //Hack
-        var but = auv.getAt(4)
-        but.setText("Edit")
+         //debugger;
+        //Todo: Detailsview not showingup after first attempt
+        console.log("User controller showEdit(Start)")
+        var auv = Ext.create("Wgo.view.AddUser");
+            auv.setValues({
+                txtUserId: record.get("id"),
+                txtUser: record.get("username"),
+                txtEmail: record.get("email"),
+                txtPwd: record.get("password")
+        });
+            //Hack
+            var but = auv.getAt(4)
+            but.setText("Edit")
 
-        this.getUserlist().push(auv);
-        this.toggleAddFormButtons()
-    console.log("User controller showEdit(End)")
-},
-toggleAddFormButtons : function(){
-    Ext.getCmp('idBtnAdd').hide();
-    Ext.getCmp('idBtnBack').show();
-    //Ext.getCmp('btnUserSubmit').setText("");
-}
+            this.getUserlist().push(auv);
+            this.toggleAddFormButtons()
+        console.log("User controller showEdit(End)")
+    },
+    toggleAddFormButtons : function(){
+        Ext.getCmp('idBtnAdd').hide();
+        Ext.getCmp('idBtnBack').show();
+        //Ext.getCmp('btnUserSubmit').setText("");
+    }
 
 
 });
