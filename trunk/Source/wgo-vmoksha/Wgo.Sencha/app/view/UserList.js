64d0e80916432ccf5b5e2114d5d89047bafa8f5b
vmmobility <vmmobility@62e6b382-633c-7ef0-cd8d-8fe3c8f30c53>
2012-08-23 11:21:30 +0000

    Modified telePhone to telephone in FestivalDetail.js Modified default value to OFF in Settings.js Removed syncing from Online Store to Offline Store in UserList.js and placed it in MainController on tap of tabBar item

--- trunk/Source/wgo-vmoksha/Wgo.Sencha/app/view/UserList.js
+++ trunk/Source/wgo-vmoksha/Wgo.Sencha/app/view/UserList.js
@@ -69,7 +69,7 @@ Ext.define('Wgo.view.UserList', {
     },
 
     initialize: function() {
-        console.log("User List Initialize (Start)")
+        console.log("User List Initialize (Start)");
         this.callParent();
         //Loading Store on page load
         //Ext.getStore('UserStore').load();
@@ -77,28 +77,7 @@ Ext.define('Wgo.view.UserList', {
         // Used when the userlist store is AllUsersStore which fetches data from local storage
         //var allUsersStore = Ext.getCmp('idUserList').getStore();
         //allUsersStore.setData(localStorage.allUsersData).load();
-        //Getting the Online Store and Offline Store 
-        var onlineStore = Ext.getStore('OnlineUserStore');
-        var offlineStore = Ext.getStore('OfflineUserStore'); 
-        //Adding an listener called 'load' to the Online Store        
-        onlineStore.addListener('load', function () {            
-            //Copying records from Online Store to Offline Store
-            this.each(function (record) {
-                //For proper syncing of data this is required
-                //when the store has unchanged data being synced the below must be used to sync the data
-                //record.phantom = true; // POST Operation
-                //Reference URL -> http://stackoverflow.com/questions/11360028/problems-with-store-syncing
-                record.dirty = true;//PUT Operation
-                //Adding data to Offline Store
-                offlineStore.add(record.data)[0];
-            });
-            //Syncing data to offline store
-            offlineStore.sync();
-            console.log(Ext.getCmp('idUserList'));
-            //Setting store to offlineStore
-            Ext.getCmp('idUserList').setStore(offlineStore);
-        });
-        onlineStore.load();
+        //Getting the Online Store and Offline Store
         console.log("User List Initialize (End)");
     }
 });
